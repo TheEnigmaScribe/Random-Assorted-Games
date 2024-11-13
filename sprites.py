fea7c1d2831354126bd90949c828b40e4afc02a6
TheEnigmaScribe <timegearkeeper@gmail.com>
2024-11-14 08:42:04 +0900

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -12,14 +12,16 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
-        Player.x = self.x
-        Player.y = self.y
+        Player.xcoord = self.x
+        Player.ycoord = self.y
         self.pdir = pdir
 
     def move(self, dx=0, dy=0):
         if not self.collide_with_walls(dx, dy) and not self.collide_with_enemy(dx, dy):
             self.x += dx
             self.y += dy
+            Player.xcoord += dx
+            Player.ycoord += dy
             #if dx or dy != 0:
                 #xval = str(self.x)
                 #yval = str(self.y)
@@ -52,9 +54,6 @@ class Player(pg.sprite.Sprite):
         self.rect.x = self.x * TILESIZE
         self.rect.y = self.y * TILESIZE
 
-    def location(x, y):
-        return (x, y)
-
 class Wall(pg.sprite.Sprite):
     def __init__(self, game, x, y):
         self.groups = game.all_sprites, game.walls
@@ -65,6 +64,8 @@ class Wall(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
+        Wall.x = self.x
+        Wall.y = self.y
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
 
@@ -75,6 +76,9 @@ class Enemy(pg.sprite.Sprite):
         img_dir = path.join(path.dirname(__file__), 'Sprites')
         self.game = game
         self.edir = edir
+        self.seeplayer = False
+        self.checkonce = 0
+        Enemy.seeplayer = self.seeplayer
         if self.edir == 1:
             eleft = pg.image.load(path.join(img_dir, "EnemyLeft.png")).convert()
             self.eleft = eleft
@@ -95,24 +99,38 @@ class Enemy(pg.sprite.Sprite):
         self.x = x
         self.y = y
         self.edir = edir
+        Enemy.xcoord = self.x
+        Enemy.ycoord = self.y
+        Enemy.direction = self.edir
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
 
     def update(self):
-        Player.location(Player.x, Player.y)
-        print(str(Player.x) + ", " + str(Player.y))
-        if (Player.x <= self.x) and (Player.y == self.y) and self.edir == 1:
-            print("playerseen")
-        if (Player.x >= self.x) and (Player.y == self.y) and self.edir == 3:
-            print("playerseen")
-        if (Player.y <= self.y) and (Player.x == self.x) and self.edir == 2:
-            print("playerseen")
-        if (Player.y >= self.y) and (Player.x == self.x) and self.edir == 4:
-            print("playerseen")
-        # add shooting functionality later
+        print(str(Player.xcoord) + ", " + str(Player.ycoord))
+        if (Player.xcoord <= self.x) and (Player.ycoord == self.y) and self.edir == 1:
+            self.wallinsightline()
+            if self.wallinsightline == False:
+                self.seeplayer = True
+        if (Player.xcoord >= self.x) and (Player.ycoord == self.y) and self.edir == 3:
+            self.seeplayer = True
+        if (Player.ycoord <= self.y) and (Player.xcoord == self.x) and self.edir == 2:
+            self.seeplayer = True
+        if (Player.ycoord >= self.y) and (Player.xcoord == self.x) and self.edir == 4:
+            self.seeplayer = True
+        if self.seeplayer == True:
+            print("spotted")
 
-    def shoot(self):
-        bullet = Bullet(x, y, edir)
+    def wallinsightline(self):
+        for wall in self.game.walls:
+            if self.edir == 1 and (wall.y == self.y) and (wall.x >= self.x and wall.x <= Player.xcoord):  
+                return True
+            if self.edir == 3 and (wall.y == self.y) and (wall.x <= self.x and wall.x >= Player.xcoord):
+                return True
+            if self.edir == 2 and (wall.x == self.x) and (wall.y >= self.y and wall.y <= Player.ycoord):
+                return True
+            if self.edir == 4 and (wall.x == self.x) and (wall.y <= self.y and wall.y >= Player.ycoord):
+                return True
+        return False
 
     def death(self):
         if (player.locationx + dx == self.x) and (player.locationy + dy == self.y):
@@ -121,13 +139,13 @@ class Enemy(pg.sprite.Sprite):
 class Bullet(pg.sprite.Sprite):
     def __init__(self, game, x, y, edir):
         self.groups = game.all_sprites, game.bullets
-        pg.sprite.Sprite.__init__(self.self.groups)
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(ORANGE)
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
-        self.dir = edir
+        self.edir = edir
         self.rect.x = x * TILESIZE
         self.rect.y = y * TILESIZE
 
